96de25bde78417df7adbcc53102ef339501a7c93
rhonal chirinos <rhonalchirinos@gmail.com>
2019-06-22 15:27:18 -0400

    node version

--- server/data-base.js
+++ server/data-base.js
@@ -19,13 +19,14 @@ if(process.env.DATABASE_URL) {
 	connection = knex({
 		client: 'pg',
 		connection: {
-			host: process.env.host, // '127.0.0.1',
-			user: process.env.user,  // 'rhonal',
-			password: process.env.password,  //'123456',
-			database: process.env.database,  // 'agrobook',
+			host:'127.0.0.1',
+			user: 'rhonal',
+			password: '123456',
+			database: 'agrobook',
 			charset: 'utf8'
 		}
 	});
 }
 
 module.exports = bookshelf(connection);
+ 
