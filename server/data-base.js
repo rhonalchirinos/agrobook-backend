3fce67dd800c33ac1d02f9a43d1158fe6ee6530a
rhonal chirinos <rhonalchirinos@gmail.com>
2019-06-22 15:01:30 -0400

    url heroku

--- server/data-base.js
+++ server/data-base.js
@@ -3,15 +3,28 @@
 const knex = require('knex');
 const bookshelf = require('bookshelf');
 
-const connection = knex({
-	client: 'pg',
-	connection: {
-		host: '127.0.0.1',
-		user: 'rhonal',
-		password: '123456',
-		database: 'agrobook',
-		charset: 'utf8'
-	}
-});
+let connection = null;
+
+if(process.env.DATABASE_URL) {
+	connection = knex({
+		client: 'pg',
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: true,
+		}
+	});
+
+} else {
+	connection = knex({
+		client: 'pg',
+		connection: {
+			host: process.env.host, // '127.0.0.1',
+			user: process.env.user,  // 'rhonal',
+			password: process.env.password,  //'123456',
+			database: process.env.database,  // 'agrobook',
+			charset: 'utf8'
+		}
+	});
+}
 
 module.exports = bookshelf(connection);
